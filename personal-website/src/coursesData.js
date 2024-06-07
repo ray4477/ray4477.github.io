d33e96f0325341c44c9d6fd4d361fc4d19f771f1
Raymond Jiang <raymondjiang2014@gmail.com>
2024-06-07 00:46:38 -0700

    added descriptions!

--- personal-website/src/coursesData.js
+++ personal-website/src/coursesData.js
@@ -3,45 +3,45 @@ const coursesBySemester = [
   {
     semester: 'Fall 2024',
     courses: [
-      { id: 1, name: 'Probability and Stochastic Processes I (ECE 381J)', description: '' },
-      { id: 2, name: 'Spoken Language Technologies (ECE 381V)', description: '' },
-      { id: 3, name: 'Wireless Communications Lab (ECE 471C)', description: '' },
-      { id: 4, name: 'Intro to Engineering Design (ECE 364D)', description: '' },
+      { id: 1, name: 'Probability and Stochastic Processes I (ECE 381J)', description: 'TBD, heard its hard...' },
+      { id: 2, name: 'Spoken Language Technologies (ECE 381V)', description: 'TBD, sounds like fun' },
+      { id: 3, name: 'Wireless Communications Lab (ECE 471C)', description: 'TBD, OFDM???🤯' },
+      { id: 4, name: 'Intro to Engineering Design (ECE 364D)', description: 'TBD, p r o j e c t' },
     ]
   },
   {
     semester: 'Spring 2024',
     courses: [
-      { id: 1, name: 'Real-time Digital Signal Processing Lab (ECE 445S)', description: '' },
-      { id: 2, name: 'Digital Signal Processing (ECE 351M)', description: '' },
-      { id: 3, name: 'Data Science Principles (ECE 461P)', description: '' },
-      { id: 4, name: 'Digital Logic (ECE 316)', description: '' },
+      { id: 1, name: 'Real-time Digital Signal Processing Lab (ECE 445S)', description: '4-QAM is cool, also baseband!!!' },
+      { id: 2, name: 'Digital Signal Processing (ECE 351M)', description: 'Kalman filter + FFTs!' },
+      { id: 3, name: 'Data Science Principles (ECE 461P)', description: 'Universal Approximators..' },
+      { id: 4, name: 'Digital Logic (ECE 316)', description: 'lol' },
     ]
   },
   {
     semester: 'Fall 2023',
     courses: [
-      { id: 1, name: 'Computer Architecture (ECE 460N)', description: '' },
-      { id: 2, name: 'Intro to Real Analysis (M 361k)', description: '' },
-      { id: 3, name: 'Data Science Lab (ECE 460J)', description: '' },
+      { id: 1, name: 'Computer Architecture (ECE 460N)', description: 'execution order of out' },
+      { id: 2, name: 'Intro to Real Analysis (M 361k)', description: 'bolzano weirstrass theorem??' },
+      { id: 3, name: 'Data Science Lab (ECE 460J)', description: 'H100 GPUs <3' },
     ]
   },
   {
     semester: 'Spring 2023',
     courses: [
-      { id: 1, name: 'Operating Systems (ECE 461S)', description: '' },
-      { id: 2, name: 'Algorithms (ECE 360C)', description: '' },
-      { id: 3, name: 'Data Structures (Software Design II) (ECE 422C)', description: '' },
-      { id: 4, name: 'Engineering Communications (ECE 333T)', description: '' },
+      { id: 1, name: 'Operating Systems (ECE 461S)', description: 'vm...' },
+      { id: 2, name: 'Algorithms (ECE 360C)', description: 'P = NP??' },
+      { id: 3, name: 'Data Structures (Software Design II) (ECE 422C)', description: 'javafx :(' },
+      { id: 4, name: 'Engineering Communications (ECE 333T)', description: 'presentations' },
     ]
   },
   {
     semester: 'Fall 2022',
     courses: [
-      { id: 1, name: 'Probability and Random Processes (ECE 351K)', description: '' },
-      { id: 2, name: 'Discrete Math (M 325K)', description: '' },
-      { id: 3, name: 'Linear Systems and Signals (ECE 313)', description: '' },
-      { id: 4, name: 'Software Design I (ECE 312)', description: '' },
+      { id: 1, name: 'Probability and Random Processes (ECE 351K)', description: 'markov chains!' },
+      { id: 2, name: 'Discrete Math (M 325K)', description: 'proof' },
+      { id: 3, name: 'Linear Systems and Signals (ECE 313)', description: 'laplace transform' },
+      { id: 4, name: 'Software Design I (ECE 312)', description: 'I <3 C coding' },
     ]
   }
 ];
